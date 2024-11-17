9d737f4a0b29204a6494594b199ad51f7f9845bc
uwaish-1 <uwaishm82@gmail.com>
2024-11-17 23:48:34 +0530

    dataTypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,28 @@
+"use strict";
+//  treat all js code as newer version
+
+// alert("hello") 
+// we are using nodejs not browser
+
+let name = "uwaish"
+let age = 20
+let isloggenin = false
+let state = null
+
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not defined
+//  symbol => unique
+
+
+// object
+
+console.log(typeof "uwaish");
+
+console.log(typeof age);
+
+console.log(typeof null);  //  object
+
+
+console.log(typeof undefined);// undefined

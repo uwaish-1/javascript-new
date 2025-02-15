48689de488ef1da9ff5e490257ea3e981c906c96
uwaish-1 <uwaishm82@gmail.com>
2025-02-15 18:15:55 +0530

    updated on 2/15/2025

---
+++ scope/02_basics.js
@@ -0,0 +1,40 @@
+// function one(){
+//     const username = "uwaish"
+
+//     function two(){
+//         const website = "youtube"
+//         console.log(username);
+        
+//     }
+//     // console.log(website);
+//     two()
+// }
+// one()
+
+// if(true){
+//     const username = "uwaish"
+//     if(username == "uwaish"){
+//         const website = "youtube"
+//         console.log(username + website);
+//     }
+// }
+
+
+
+// +++++++++++++++++++++
+
+//  right
+console.log(addone(5));
+
+function addone(num){
+    return num + 1
+}
+
+
+//  wrong 
+console.log(addtwo(5));
+
+const addtwo = function(num){
+    return num + 2
+}
+
